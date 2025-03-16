477eb57194eaa1250603d372dc5a192245cae96c
BlazeDashX <refat00021@gmail.com>
2025-03-16 12:28:43 +0600

    Terms up

--- views/terms.php
+++ views/terms.php
@@ -7,7 +7,7 @@
 <body>
     <h2>Terms and Conditions</h2>
     <p>By registering on our freelance platform, you agree to abide by our terms and conditions. You must provide accurate information, respect other users, and comply with applicable laws. We are not responsible for disputes between users. Payments and transactions should be handled securely through verified methods. Any violation of these terms may result in account suspension.</p>
-    
+    <h2>Terms and Conditions</h2>
     <p><a href="index.php">Back to Home</a></p>
 </body>
 
